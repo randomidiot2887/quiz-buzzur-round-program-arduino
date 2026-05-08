e9d6868364673059b640db8f48c7988da910f3b2
randomidiot2887 <aliaikmohammedshameem@gmail.com>
2026-05-08 12:11:42 +0500

    cleaned up a bit, fully moved procedures over to procedures.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -20,12 +20,6 @@ const int button[4]={4, 5, 6, 7};
 const int led[4]={8, 9, 10, 11};
 const int baud=9600; // Change to change the baud of the serial output of the arduino to match with the serial console
 
-// ==========
-// FUNCTIONS
-// ==========
-// - Beep (beep the buzzur 1 time for 100ms, halts the program while running)
-
-
 
 /*
 Setup code that
@@ -52,51 +46,4 @@ void setup(){
 }
 
 
-void loop(){
-  
-}
-/*
-// inititlaiing all 4 leds
-const int led1=8;
-const int led2=9;
-const int led3=10;
-const int led4=11;
-// initialising all 4 buttons
-const int button1=4;
-const int button2=5;
-const int button3=6;
-const int button4=7;
-
-
-// Below is the code that runs one during setup
-void setup() {
-  // Initialising serial connection at 9600 baud
-  Serial.begin(9600);
-  // Initialising the leds as outputs
-  pinMode(button1, INPUT_PULLUP);
-  pinMode(button2, INPUT_PULLUP);
-  pinMode(button3, INPUT_PULLUP);
-  pinMode(button4, INPUT_PULLUP);
-
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  pinMode(led4, OUTPUT);
-
-  digitalWrite(led1, HIGH);
-  digitalWrite(led2, HIGH);
-  digitalWrite(led3, HIGH);
-  digitalWrite(led4, HIGH);
-
-  Serial.println("Ready to hear button presses");
-}
-
-
-// Below is the code to be looped
-void loop() {
-  if (digitalRead(button2) == LOW) {
-    Serial.println("HE TOUCHED THE rrrrrrr BUTTON");
-    
-  }
-}
-*/
+void loop(){}
--- src/procedures.cpp
+++ src/procedures.cpp
@@ -1,6 +1,9 @@
 #include "procedures.h"
 
-
+// ==========
+// Procedures required for the program are stored here
+// ==========
+// - Beep (beep the buzzur 1 time for 100ms, halts the program while running)
 void beep(){
   digitalWrite(buzzur, HIGH);
   delay(100);
